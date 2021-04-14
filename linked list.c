0c243cbeb06e1150145008eeb10fd8f811f0e40f
Ullyanne <ujfp@ic.ufal.br>
2021-04-14 00:44:10 -0300

    print, bubblesort and swap functions added

--- linked list.c
+++ linked list.c
@@ -57,6 +57,57 @@ void addEnd_tail(struct node** head, struct node** tail, int item)
     *tail = newnode;
 }
 
+void print(struct node* head)
+{
+    struct node* ptr = head;
+
+    while(ptr != NULL)
+    {
+        if(ptr->next == NULL)
+            printf("%d", ptr->item);
+        else
+            printf("%d ", ptr->item);
+        ptr = ptr->next;
+    }
+}
+
+void swap(struct node* x, struct node* y)
+{
+    int item;
+
+    item = x->item;
+    x->item = y->item;
+    y->item = item;
+}
+
+void bubblesort(struct node* first)
+{
+    struct node* ptr;
+    struct node* last = NULL;
+    int swapped = 0;
+
+    if(first == NULL)
+        return;
+
+    do
+    {
+        ptr = first;
+        swapped = 0;
+
+        while(ptr->next != last)
+        {
+            if(ptr->item > ptr->next->item)
+            {
+                swap(ptr, ptr->next);
+                swapped = 1;
+            }
+            
+            ptr = ptr->next;
+        }
+        
+        last = ptr;
+    } while (swapped);
+}
 
 int main()
 {
