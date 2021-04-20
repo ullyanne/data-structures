19de95c84c452c2574182ff2be08a5467915fd47
Ullyanne <ujfp@ic.ufal.br>
2021-04-20 04:53:47 -0300

    queue and priority queue added

---
+++ priorityqueue.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct nodeP
+{
+    int item;
+    int priority;
+    struct nodeP* next;
+};
+
+struct queueP
+{
+    struct nodeP* head;
+    int size;
+};
+
+struct nodeP* createnodeP(int item, int priority)
+{
+    struct nodeP* newnode = (struct nodeP*) malloc(sizeof(struct nodeP));
+    newnode->item = item;
+    newnode->priority = priority;
+
+    return newnode;
+}
+
+struct queueP* createqueueP()
+{
+    struct queueP* newqueue = (struct queueP*) malloc(sizeof(struct queueP));
+    newqueue->head = NULL;
+    newqueue->size = 0;
+    return newqueue;
+}
+
+bool isEmptyP(struct queueP* queue)
+{
+    return queue->size == 0;
+}
+
+void enqueueP(struct queueP** queue, int item, int priority)
+{
+    struct nodeP* newnode = createnodeP(item, priority);
+    (*queue)->size++;
+
+    if((*queue)->head == NULL)
+    {
+        newnode->next = (*queue)->head;
+        (*queue)->head = newnode;
+        return;
+    }
+    else if(priority > (*queue)->head->priority)
+    {
+        newnode->next = (*queue)->head;
+        (*queue)->head = newnode;
+        return;
+    }
+    
+    struct nodeP* current = (*queue)->head;
+
+    while(current->next != NULL && priority < current->next->priority)
+    {
+        current = current->next;
+    }
+    newnode->next = current->next;
+    current->next = newnode;
+}
+
+struct nodeP* dequeue(struct queueP** queue)
+{
+    if(isEmptyP(*queue))
+        printf("Queue underflow.\n");
+    else
+    {
+        struct nodeP* dequeued = (*queue)->head;
+        
+        (*queue)->head = (*queue)->head->next;
+        (*queue)->size--;
+        
+        return dequeued;
+    }
+}
+
+void printP(struct queueP* queue)
+{
+    struct nodeP* ptr = queue->head;
+    if(isEmptyP(queue))
+        printf("Empty queue.");
+    
+    while(ptr!= NULL)
+    {
+        printf("%d ", ptr->priority);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    struct queueP* queueP = createqueueP();
+    
+    free(queueP);
+}
---
+++ queue.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node
+{
+    int item;
+    struct node* next;
+};
+
+struct queue
+{
+    struct node* head;
+    struct node* tail;
+    int size;
+};
+
+struct node* createNode(int item)
+{
+    struct node* newnode = (struct node*) malloc(sizeof(struct node));
+    newnode->item = item;
+
+    return newnode;
+}
+
+struct queue* createQueue()
+{
+    struct queue* newqueue = (struct queue*) malloc(sizeof(struct queue));
+    newqueue->head = NULL;
+    newqueue->tail = NULL;
+    newqueue->size = 0;
+    return newqueue;
+}
+
+bool isEmpty(struct queue* queue)
+{
+    return queue->size == 0;
+}
+
+void enqueue(struct queue** queue, int item)
+{
+    struct node* newtail = createNode(item);
+    (*queue)->size++;
+
+    if((*queue)->head == NULL)
+    {
+        (*queue)->head = newtail;
+        (*queue)->tail = newtail;
+        return;
+    }
+    
+    (*queue)->tail->next = newtail;
+    (*queue)->tail = newtail;
+}
+
+struct node* dequeue(struct queue** queue)
+{
+    if(isEmpty(*queue))
+        printf("Queue underflow.\n");
+    else
+    {
+        struct node* dequeued = (*queue)->head;
+        
+        (*queue)->head = (*queue)->head->next;
+        (*queue)->size--;
+
+        if((*queue)->head == NULL)
+            (*queue)->tail == NULL;
+        
+        return dequeued;
+    }
+}
+
+void print(struct queue* queue)
+{
+    struct node* ptr = queue->head;
+    if(isEmpty(queue))
+        printf("Empty queue.");
+    
+    while(ptr!= NULL)
+    {
+        printf("%d ", ptr->item);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    struct queue* queue = createQueue();
+
+    free(queue);
+}
